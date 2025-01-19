d8c9830899cf2fe2578ba2766d67beca04932b37
thiro <thiro@poczta.onet.pl>
2025-01-19 17:24:19 +0100

    conan'izing RCF 2.2.0.0

---
+++ RCF-2.2.0.0/conanfile.py
@@ -0,0 +1,56 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class rcfRecipe(ConanFile):
+    name = "rcf"
+    version = "2.2.0.0"
+    package_type = "library"
+
+    # Optional metadata
+    license = "GPL v2 license"
+    author = "Delta V Software"
+    url = "https://github.com/thirogit/bk3rdparty"
+    description = "Remote Call Framework"
+    topics = ("RPC", "IPC")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+    
+    def requirements(self):
+        self.requires("boost/1.83.0")
+        
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.rm_safe("fPIC")
+
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+
+    def layout(self):
+        cmake_layout(self)
+    
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["rcf"]
+
